2cd23386b9822e4bb4d04892c4920081af5c489f
Mattias Andersson <mattias@webnyans.se>
2015-09-27 23:23:30 +0200

    Making lists sortable and expandable
    - Inserting css classes to mark list item
      expand/collapse status, then using css
      to hide/show
    - Inserting up/down arrow in list which user
      can click to sort in ascending/descending
      order

--- src/themes/sony/functions.php
+++ src/themes/sony/functions.php
@@ -1,2 +1,20 @@
 <?php
+	// Add js
+	function ssm_theme_js() {
+		// Add custom subpage handler
+		$path = get_stylesheet_directory_uri() . '/js/subpage_handler.js';
+		$dependencies = array( 'jquery' );
+		wp_enqueue_script( 'subpage_handler_js', $path, $dependencies, true );
+	}
+	add_action( 'wp_enqueue_scripts', 'ssm_theme_js' );
+
+	// Add css
+	function ssm_theme_css() {
+		// Add dashicons
+		wp_enqueue_style( 'dashicons' );
+	}
+	add_action( 'wp_enqueue_scripts', 'ssm_theme_css' );
+
+	// Add thumbnail support
+	add_theme_support( 'post-thumbnails' );
 ?>
---
+++ src/themes/sony/js/subpage_handler.js
@@ -0,0 +1,123 @@
+
+/**
+ * Js to make subpage hierarchy list sortable, expandable and collapsible
+ */
+
+jQuery(document).ready(function($){
+	
+	main();
+
+	/**
+	 * Main function
+	 */
+	function main() {
+		collapseLists();
+		makeListsExpandable();
+		sortLists();
+		makeListsSortable();
+	}
+	
+
+	/**
+	 * Function that collapses all subpage lists
+	 */
+	function collapseLists() {
+		$('.subpageList').find('li:has(ul)')
+			.addClass('collapsed')
+			.children('ul').hide();
+	}
+
+
+	/**
+	 * Function that makes list expandable/collapsable
+	 */
+	function makeListsExpandable() {
+		// Find all expandable lists and add click event
+		$('.subpageList').find('li:has(ul)')
+			.click( function(event) {
+				if (this == event.target) {
+					$(this).toggleClass('expanded');
+					$(this).children('ul').toggle('medium');
+				}
+				return false;
+			}); 	
+	};
+
+
+	/**
+	 * Function that sorts all available subpage lists in ascending order
+	 */
+	function sortLists() {
+		// Loop all ul, and for each list do initial sort
+		var ul = $(".subpageListContainer ul");
+		$.each(ul, function() {
+			sortList($(this), false);
+		});
+	}
+
+
+	/**
+	 * Function that adds click events to make lists sortable
+	 * in ascending or descending order
+	 */
+	function makeListsSortable() {
+		// Get all lists
+		var ul = $(".subpagelistContainer ul");
+		
+		// Add click events on each sort arrow, toggle between ascending and descending order
+		$.each(ul, function() {
+			$(this).find('.ssm-list-sort').click(function(e) {
+				if($(e.target).hasClass('dashicons-arrow-up-alt2')) {
+					// Show down icon and sort list in descending order
+					var sortArrow = $(e.target);
+					sortArrow.removeClass('dashicons-arrow-up-alt2');
+					sortArrow.addClass('dashicons-arrow-down-alt2');
+					sortList($(sortArrow.parent()), false);
+				} else {
+					// Show up icon and sort list in ascending order
+					var sortArrow = $(e.target);
+					sortArrow.addClass('dashicons-arrow-up-alt2');
+					sortArrow.removeClass('dashicons-arrow-down-alt2');
+					sortList($(sortArrow.parent()), true);
+				}
+			})
+		});
+	}
+
+
+	/**
+	 * Function to sort list
+	 * Note: Sort arrow included in list so needs to be sorted first in list
+	 *
+	 * @param bool asc - Whether or not to sort in ascending order
+	 */
+	function sortList(ul, asc) {
+		// Sort ascending or descending
+		var sortFlag = asc ? 1 : -1;
+
+		// Get the list items as array
+		var arr = $.makeArray(ul.children("li"));
+
+		// Sort the array
+		arr.sort(function(a, b) {
+				var textA = $(a).text();
+				var textB = $(b).text();
+
+				if($(a).hasClass('ssm-list-sort')) return -1;  // Sort arrow always top of list
+				if(textA < textB) return -sortFlag;
+				if(textA > textB) return sortFlag;
+
+				return 0;
+		});
+
+		// Empty list and instead append the sorted list
+		ul.empty();
+		$.each(arr, function() {
+				ul.append(this);
+		});
+	
+		// Make lists expandable and sortable again after removing/adding elements
+		makeListsExpandable();
+		makeListsSortable();
+	}	
+})
