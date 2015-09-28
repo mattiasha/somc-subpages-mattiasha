dd63bf98fc51ab50268d84130265d76fd62a58a3
Mattias Andersson <mattias@webnyans.se>
2015-09-28 10:14:34 +0200

    Code cleanup
    - Making indentation size 2 and tabs, not spaces, for
      plugin file

--- src/plugins/somc-subpages-mattiasha/somc-subpages-mattiasha.php
+++ src/plugins/somc-subpages-mattiasha/somc-subpages-mattiasha.php
@@ -39,7 +39,7 @@ class Ssm_Somc_Subpages {
 	 */
 	public function ssm_shortcode($args, $content = '') {
 		return Subpage_Hierarchy::get_hierarchy();
-  }
+	}
 }
 
 
@@ -50,7 +50,7 @@ class Ssm_Somc_Subpages {
  * Function to register widgets
  */
 function ssm_register_widgets() {
-  register_widget( 'Ssm_Somc_Subpages_Widget' );
+	register_widget( 'Ssm_Somc_Subpages_Widget' );
 }
 
 
@@ -59,72 +59,72 @@ function ssm_register_widgets() {
  */ 
 class Ssm_Somc_Subpages_Widget extends WP_Widget {
 
-  /** 
-   * Constructor
-   */
-  function __construct() {
-    parent::__construct(
-      // Base ID
-      'Ssm_Somc_Subpages_Widget', 
+	/** 
+	 * Constructor
+	 */
+	function __construct() {
+		parent::__construct(
+			// Base ID
+			'Ssm_Somc_Subpages_Widget', 
 
-      // Widget name
-      __('Subpage Hierarchy Widget', 'ssm_widget_domain'), 
+			// Widget name
+			__('Subpage Hierarchy Widget', 'ssm_widget_domain'), 
 
-      // Widget description
-      array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'ssm_widget_domain' ), ) 
-    );
-  }
+			// Widget description
+			array( 'description' => __( 'Sample widget based on WPBeginner Tutorial', 'ssm_widget_domain' ), ) 
+		);
+	}
 
-  /**
-   * Widget front-end
-   */
-  public function widget( $args, $instance ) {
-    // Before widget
-    echo $args['before_widget'];
-    
-    // Title
-    $title = apply_filters( 'widget_title', $instance['title'] );
-    if ( ! empty( $title ) ) {
-      echo $args['before_title'] . $title . $args['after_title'];
-    }
-    
-    // Widget content
-    echo Subpage_Hierarchy::get_hierarchy();
-    
-    // After widget
-    echo $args['after_widget'];
-  }
-      
-  
-  /**
-   * Widget backend
-   */ 
-  public function form( $instance ) {
-    // Title
-    if ( isset( $instance[ 'title' ] ) ) {
-      $title = $instance[ 'title' ];
-    }
-    else {
-      $title = __( 'New title', 'ssm_widget_domain' );
-    }
-    
-    // Widget admin form
-    ?>
-      <p>
-      <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
-      <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
-      </p>
-    <?php 
-  }
-    
-  /**
-   * Widget update
-   */
-  public function update( $new_instance, $old_instance ) {
-    $instance = array();
-    $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
-    return $instance;
-  }
+	/**
+	 * Widget front-end
+	 */
+	public function widget( $args, $instance ) {
+		// Before widget
+		echo $args['before_widget'];
+		
+		// Title
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+		
+		// Widget content
+		echo Subpage_Hierarchy::get_hierarchy();
+		
+		// After widget
+		echo $args['after_widget'];
+	}
+			
+	
+	/**
+	 * Widget backend
+	 */ 
+	public function form( $instance ) {
+		// Title
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'ssm_widget_domain' );
+		}
+		
+		// Widget admin form
+		?>
+			<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			</p>
+		<?php 
+	}
+		
+	/**
+	 * Widget update
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
 } 
 
 
@@ -135,76 +135,76 @@ class Ssm_Somc_Subpages_Widget extends WP_Widget {
  * Helper class for creating page hierarchy
  */
 class Subpage_Hierarchy {
-  /**
-   * Function that creates shortcode that lists hierarchical subpage structure
-   * of current page
-   */
-  public static function get_hierarchy() {
-    // Get child hierarchy of current page
-    global $post;
-    $current_page = get_post($post->ID);
-    self::create_hierarchy($current_page);  // Adds 'children' property to $current_page
-    
-    // Create html output for page structure, using the new 'children' property
-    $output = '<div class="subpagelistContainer">';
-    $level = 0;
-    if ( count ( $current_page->children ) > 0 && $current_page->children != NULL ) {
-      self::create_hierarchy_html($current_page->children, $output, $level);
-    }
-    else {
-      $output .= 'No subpages found';
-    }
-    // self::create_hierarchy_html($current_page->children, $output, $level);
-    $output .= "</div>";
+	/**
+	 * Function that creates shortcode that lists hierarchical subpage structure
+	 * of current page
+	 */
+	public static function get_hierarchy() {
+		// Get child hierarchy of current page
+		global $post;
+		$current_page = get_post($post->ID);
+		self::create_hierarchy($current_page);  // Adds 'children' property to $current_page
+		
+		// Create html output for page structure, using the new 'children' property
+		$output = '<div class="subpagelistContainer">';
+		$level = 0;
+		if ( count ( $current_page->children ) > 0 && $current_page->children != NULL ) {
+			self::create_hierarchy_html($current_page->children, $output, $level);
+		}
+		else {
+			$output .= 'No subpages found';
+		}
+		// self::create_hierarchy_html($current_page->children, $output, $level);
+		$output .= "</div>";
 
-    return $output;
-  }
-
-
-  /**
-   * Function that creates hierarchical list structure corresponding
-   * to the hierarchical page structure
-   *
-   * @param array $pages      - The array of page objects for one specific level
-   * @param string $output    - the resulting html
-   * @param int $level            - The recursion level
-   */
-  private static function create_hierarchy_html($pages, &$output, $level) {
-    // Only add expList id on first level of list
-    $id = $level == 0 ? " class='subpageList'" : '';
-
-    $output .= "<ul$id>";
-
-    // Sort icon
-    $output .= '<li class="ssm-list-sort dashicons dashicons-arrow-up-alt2"></li>'; 
-    // <span class="dashicons dashicons-arrow-up-alt2"></span>
-
-    // Loop through all pages and add info in list
-    foreach ( $pages as $page) {
-      $output .= '<li>'; 
-
-      // The page title, truncated after 20 chars
-      $output .= mb_substr($page->post_title, 0, 20);
-
-      // The page thumbnail
-      $thumbnail_size = array(15, 15);
-      $output .=  get_the_post_thumbnail( $page->ID, $thumbnail_size );
-
-      // If page has children, add sublist recursively
-      if ( count($page->children) > 0 && $page->children != NULL ) {
-        self::create_hierarchy_html($page->children, $output, $level + 1);
-      }
-
-      $output .= '</li>';
-    }
-
-    $output .= '</ul>';
-
-    // $output .= '<div class="ssm-list-sort">sort></div>';
-  }
+		return $output;
+	}
 
 
-  
+	/**
+	 * Function that creates hierarchical list structure corresponding
+	 * to the hierarchical page structure
+	 *
+	 * @param array $pages      - The array of page objects for one specific level
+	 * @param string $output    - the resulting html
+	 * @param int $level            - The recursion level
+	 */
+	private static function create_hierarchy_html($pages, &$output, $level) {
+		// Only add expList id on first level of list
+		$id = $level == 0 ? " class='subpageList'" : '';
+
+		$output .= "<ul$id>";
+
+		// Sort icon
+		$output .= '<li class="ssm-list-sort dashicons dashicons-arrow-up-alt2"></li>'; 
+		// <span class="dashicons dashicons-arrow-up-alt2"></span>
+
+		// Loop through all pages and add info in list
+		foreach ( $pages as $page) {
+			$output .= '<li>'; 
+
+			// The page title, truncated after 20 chars
+			$output .= mb_substr($page->post_title, 0, 20);
+
+			// The page thumbnail
+			$thumbnail_size = array(15, 15);
+			$output .=  get_the_post_thumbnail( $page->ID, $thumbnail_size );
+
+			// If page has children, add sublist recursively
+			if ( count($page->children) > 0 && $page->children != NULL ) {
+				self::create_hierarchy_html($page->children, $output, $level + 1);
+			}
+
+			$output .= '</li>';
+		}
+
+		$output .= '</ul>';
+
+		// $output .= '<div class="ssm-list-sort">sort></div>';
+	}
+
+
+	
 
 	/**
 	 * Function that adds a 'children' property to a page WP_Post object, where
